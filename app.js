675e3945b375905493e0ff85c537644bada6852d
Karabet Jazmahji <48636226+KarabetJazmahji@users.noreply.github.com>
2020-08-10 01:16:33 -0400

    try1

--- app.js
+++ app.js
@@ -413,7 +413,7 @@ if (process.env.NODE_ENV === 'production') {
 //   }
 
 
-app.set('port', (process.env.PORT || 8800));
+app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), () => {
     console.log('Node app is running on port', app.get('port'));
 })
